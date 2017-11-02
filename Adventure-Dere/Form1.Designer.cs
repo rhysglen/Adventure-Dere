d4f13fdbd0bc98f12865d094ee0c0f6d2ce407fe
temp <temp>
2017-11-02 13:13:45 -0400

    Add project files.

---
+++ Adventure-Dere/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Adventure_Dere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.question2Label = new System.Windows.Forms.Label();
+            this.question3Label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 76);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adventure-Dere";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(28, 85);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(336, 177);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.Text = "label2";
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.Location = new System.Drawing.Point(12, 328);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(352, 42);
+            this.questionLabel.TabIndex = 2;
+            this.questionLabel.Text = "label2";
+            // 
+            // question2Label
+            // 
+            this.question2Label.Location = new System.Drawing.Point(12, 388);
+            this.question2Label.Name = "question2Label";
+            this.question2Label.Size = new System.Drawing.Size(352, 42);
+            this.question2Label.TabIndex = 3;
+            this.question2Label.Text = "label3";
+            // 
+            // question3Label
+            // 
+            this.question3Label.Location = new System.Drawing.Point(12, 444);
+            this.question3Label.Name = "question3Label";
+            this.question3Label.Size = new System.Drawing.Size(352, 42);
+            this.question3Label.TabIndex = 4;
+            this.question3Label.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(738, 495);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.question3Label);
+            this.Controls.Add(this.question2Label);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Adventure-Dere";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Label question2Label;
+        private System.Windows.Forms.Label question3Label;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Adventure-Dere/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adventure_Dere
+{
+    public partial class Form1 : Form
+    {
+       // variable to tell where the user is
+        int scene = 0;
+
+        // random number generator
+        Random randGen = new Random();
+        int scene2Chance,scene1Chance;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // initial output // start of the game// random values
+            scene1Chance = randGen.Next(1, 101);
+            scene2Chance = randGen.Next(1, 101);
+            button1.Visible = false;
+            outputLabel.Text = "You are a boy in your second year of high school.";
+            outputLabel.Text += "The school dance is coming up and you do not have a date. ";
+            outputLabel.Text += "There are two girls in your class who also do not have a date. ";
+            outputLabel.Text += "Which one do you talk to?";
+            questionLabel.Text = "Girl 1";
+            question2Label.Text = "Girl 2";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // tells where the game to go when a certain button is pressed.
+            if (e.KeyCode == Keys.M)
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 12; }
+                else if (scene == 12) { scene = 13; }
+                else if (scene == 13) { scene = 15; }
+                else if (scene == 4) { scene = 5; }
+                else if (scene == 6) { scene = 6; }
+                else if (scene == 7) { scene = 8; }
+                else if (scene == 9) { scene = 10; }
+                else if (scene == 10) { scene = 8; }
+                else if (scene == 11) { scene = 5; }
+                else if (scene == 2) { scene = 16; }
+                else if (scene == 16) { scene = 17; }
+                else if (scene == 18) { scene = 19; }
+                else if (scene == 20) { scene = 21; }
+                else if (scene == 22) { scene = 23; }
+                else if (scene == 25) { scene = 26; }
+            }
+            else if (e.KeyCode == Keys.B)
+            {
+                if (scene == 0) { scene = 2; }
+                else if (scene == 2)
+                {
+                    if (scene2Chance > 50)
+                    {
+                        scene = 25;
+                    }
+                    else
+                    {
+                        scene = 26;
+                    }
+                }
+                else if (scene == 25) { scene = 0; }
+                else if (scene == 1)
+                {
+                    if (scene1Chance < 10)
+                    {
+                        scene = 3;
+                    }
+                    else
+                    {
+                        scene = 4;
+                    }
+                }
+                else if (scene == 6) { scene = 9; }
+                else if (scene == 9) { scene = 11; }
+                else if (scene == 12) { scene = 14; }
+                else if (scene == 13) { scene = 3; }
+                else if (scene == 16) { scene = 18; }
+                else if (scene == 18) { scene = 20; }
+                else if (scene == 20) { scene = 22; }
+                else if (scene == 22) { scene = 24; }
+            }
+            else if (e.KeyCode == Keys.N)
+            {
+                if (scene == 1) { scene = 2; }
+                else if (scene == 2) { scene = 1; }
+                else if (scene == 27) {scene = 0; }
+                else if (scene == 5) { scene = 0; }
+                else if (scene == 8) { scene = 0; }
+                else if (scene == 14) { scene = 0; }
+                else if (scene == 15) { scene = 0; }
+                else if (scene == 21) { scene = 0; }
+                else if (scene == 23) { scene = 0; }
+                else if (scene == 17) { scene = 0; }
+                else if (scene == 19) { scene = 0; }
+                else if (scene == 24) { scene = 0; }
+                else if (scene == 26) { scene = 0; }
+
+
+            }
+            // the dialouge of the scenes and the options for the player
+            switch (scene)
+            {
+                case 0:
+                    outputLabel.Text = "You are a boy in your second year of high school.";
+                    outputLabel.Text += "The school dance is coming up and you do not have a date. ";
+                    outputLabel.Text += "There are two girls in your class who also do not have a date. ";
+                    outputLabel.Text += "Which one do you talk to?";
+                    questionLabel.Text = "Girl 1";
+                    question2Label.Text = "Girl 2";
+                    break;
+                case 1:
+                    outputLabel.Text = "You Approach the girl and she greets you with a smile.";
+                    questionLabel.Text = "Ask her on a date?";
+                    question2Label.Text = "Ask her to the dance?";
+                    question3Label.Text = "Talk to the other girl.";
+                    break;
+                case 2:
+                    outputLabel.Text = "You Approach the girl and she glares at you.";
+                    questionLabel.Text = "Ask her out?";
+                    question2Label.Text = " Walk Away?";
+                    question3Label.Text = "Talk to the other girl";
+                    break;
+                case 3:
+                    outputLabel.Text = "She smiles and gives a pleasant laugh as she agrees to your proposal. " +
+                        "she says to pick her up by 7. you rush home excited as you prepare for the dance.";
+                    questionLabel.Text = "Continue.";
+                    break;
+                case 4:
+                    outputLabel.Text = "She smiles at you but she politely turns you down. " +
+                        "she says that she doesn't know you that well and that she would feel " +
+                        "uncomfortable at a dance with a person she has never spent any time with prior. ";
+                    questionLabel.Text = "Continue.";
+                    break;
+                case 5:
+                    outputLabel.Text = "You turn around defeated as you go home. You see the other girl on your way and she smiles at you." +
+                        " You are too depressed to do anything else and you retreat to your room for the rest of the year. ";
+                    question3Label.Text = "Press 'n' to Restart.";
+                    break;
+                case 1:
+                    outputLabel.Text =
+                    questionLabel.Text =
+                    question2Label.Text =
+                    question3Label.Text =
+                    break;
+                case 1:
+                    outputLabel.Text =
+                    questionLabel.Text =
+                    question2Label.Text =
+                    question3Label.Text =
+                    break;
+                case 1:
+                    outputLabel.Text =
+                    questionLabel.Text =
+                    question2Label.Text =
+                    question3Label.Text =
+                    break;
+                case 1:
+                    outputLabel.Text =
+                    questionLabel.Text =
+                    question2Label.Text =
+                    question3Label.Text =
+                    break;
+                case 1:
+                    outputLabel.Text =
+                    questionLabel.Text =
+                    question2Label.Text =
+                    question3Label.Text =
+                    break;
+                case 1:
+                    outputLabel.Text =
+                    questionLabel.Text =
+                    question2Label.Text =
+                    question3Label.Text =
+                    break;
+
+
+            }
+                
+
+            }
+    }
+}
